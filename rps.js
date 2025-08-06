158e464b09ffd16b63f7f0d9ed6bfe724633237d
Rhiannon <154023480+RSpencerosa@users.noreply.github.com>
2025-08-05 19:21:18 -0700

    Add UI

--- rps.js
+++ rps.js
@@ -9,58 +9,81 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Rock, Paper, Scissors?").toLowerCase()
-}
+//function getHumanChoice() {
+  //  return prompt("Rock, Paper, Scissors?").toLowerCase()
+//}
 
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == "rock") {
-            if (computerChoice == "scissors") {
-                console.log("You win! Rock beats Scissors!");
-                humanScore++
-            } else if (computerChoice == "paper") {
-                console.log("You lose! Paper beats Rock!");
-                computerScore++     
-            } else if (computerChoice == "rock") {
-                console.log("Draw! Rock vs. Rock!")
-            }
-        } else if (humanChoice = "scissors") {
-            if (computerChoice == "paper") {
-                console.log("You win! Scissors beats Paper!");
-                humanScore++
-            } else if (computerChoice == "rock") {
-                console.log("You lose! Rock beats Scissors!");
-                computerScore++     
-            } else if (computerChoice == "scissors") {
-                console.log("Draw! Scissors vs. Scissors!")
-            }
-        } else if (humanChoice = "paper") {
-            if (computerChoice == "rock") {
-                console.log("You win! Paper beats Rock!");
-                humanScore++
-            } else if (computerChoice == "scissors") {
-                console.log("You lose! Scissors beats Paper!");
-                computerScore++     
-            } else if (computerChoice == "paper") {
-                console.log("Draw! Paper vs. Paper!")  
-            }
+
+function playRound(humanChoice, computerChoice, place) {
+    const results = document.createElement("p")
+    if (humanChoice == "rock") {
+        if (computerChoice == "scissors") {
+            results.textContent ="You win! Rock beats Scissors!"
+            humanScore++
+        } else if (computerChoice == "paper") {
+            results.textContent ="You lose! Paper beats Rock!"
+            computerScore++     
+        } else if (computerChoice == "rock") {
+            results.textContent ="Draw! Rock vs. Rock!"
+        }
+    } else if (humanChoice = "scissors") {
+        if (computerChoice == "paper") {
+            results.textContent ="You win! Scissors beats Paper!"
+            humanScore++
+        } else if (computerChoice == "rock") {
+           results.textContent ="You lose! Rock beats Scissors!"
+            computerScore++     
+        } else if (computerChoice == "scissors") {
+            results.textContent ="Draw! Scissors vs. Scissors!"
+        }
+    } else if (humanChoice = "paper") {
+        if (computerChoice == "rock") {
+            results.textContent ="You win! Paper beats Rock!"
+            humanScore++
+        } else if (computerChoice == "scissors") {
+            results.textContent ="You lose! Scissors beats Paper!"
+            computerScore++     
+        } else if (computerChoice == "paper") {
+            results.textContent ="Draw! Paper vs. Paper!"
         }
     }
-
-    let humanScore = 0
-    let computerScore = 0
-
-    for (let n = 0; n < 5; n++) {
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection)
-    }
-
-    console.log(humanScore)
-    console.log(computerScore)
+    place.appendChild(results)
 }
 
+function updateScore(place,human,computer) {
+    place.textContent = `Player: ${human} | Computer: ${computer}`
+    if (humanScore == 5) {
+    scores.textContent = "WINNER!"
+    }
+    if (computerScore == 5) {
+    scores.textContent = "LOSER!"
+    }
+}
+
+let humanScore = 0
+let computerScore = 0
+
+const scores = document.querySelector("#score")
+
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
+
+const outcome = document.querySelector("#outcome")
+
+rock.addEventListener("click", function() {
+    playRound("rock",getComputerChoice(),outcome)
+    updateScore(scores,humanScore,computerScore)
+    
+})
+paper.addEventListener("click", function() {
+    playRound("paper",getComputerChoice(),outcome)
+    updateScore(scores,humanScore,computerScore)
+})
+scissors.addEventListener("click", function() {
+    playRound("scissors",getComputerChoice(),outcome)
+    updateScore(scores,humanScore,computerScore)
+})
 
 
-playGame()
+
